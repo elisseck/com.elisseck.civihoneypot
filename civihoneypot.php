c523764c2e7195406a3fd01564ff5a81e7099d6b
Eli Lisseck <eli@aghstrategies.com>
2023-01-13 14:15:38 -0800

    php8

--- civihoneypot.php
+++ civihoneypot.php
@@ -131,15 +131,6 @@ function civihoneypot_civicrm_config(&$config) {
 }
 
 /**
- * Implements hook_civicrm_xmlMenu().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_xmlMenu
- */
-function civihoneypot_civicrm_xmlMenu(&$files) {
-  _civihoneypot_civix_civicrm_xmlMenu($files);
-}
-
-/**
  * Implements hook_civicrm_install().
  *
  * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_install
@@ -193,54 +184,6 @@ function civihoneypot_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
   return _civihoneypot_civix_civicrm_upgrade($op, $queue);
 }
 
-/**
- * Implements hook_civicrm_managed().
- *
- * Generate a list of entities to create/deactivate/delete when this module
- * is installed, disabled, uninstalled.
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_managed
- */
-function civihoneypot_civicrm_managed(&$entities) {
-  _civihoneypot_civix_civicrm_managed($entities);
-}
-
-/**
- * Implements hook_civicrm_caseTypes().
- *
- * Generate a list of case-types.
- *
- * Note: This hook only runs in CiviCRM 4.4+.
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_caseTypes
- */
-function civihoneypot_civicrm_caseTypes(&$caseTypes) {
-  _civihoneypot_civix_civicrm_caseTypes($caseTypes);
-}
-
-/**
- * Implements hook_civicrm_angularModules().
- *
- * Generate a list of Angular modules.
- *
- * Note: This hook only runs in CiviCRM 4.5+. It may
- * use features only available in v4.6+.
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_angularModules
- */
-function civihoneypot_civicrm_angularModules(&$angularModules) {
-  _civihoneypot_civix_civicrm_angularModules($angularModules);
-}
-
-/**
- * Implements hook_civicrm_alterSettingsFolders().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_alterSettingsFolders
- */
-function civihoneypot_civicrm_alterSettingsFolders(&$metaDataFolders) {
-  _civihoneypot_civix_civicrm_alterSettingsFolders($metaDataFolders);
-}
-
 // --- Functions below this ship commented out. Uncomment as required. ---
 
 /**
@@ -248,9 +191,8 @@ function civihoneypot_civicrm_alterSettingsFolders(&$metaDataFolders) {
  *
  * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_preProcess
  *
-function civihoneypot_civicrm_preProcess($formName, &$form) {
 
-} // */
+ // */
 
 /**
  * Implements hook_civicrm_navigationMenu().
